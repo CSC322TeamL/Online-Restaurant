883b9a2fb5e20532ac7eb318b7b36926d37a4586
Weiye Kuang <56404037+wkuang000@users.noreply.github.com>
2020-12-05 16:04:23 -0500

    Update demoSever.py

--- demoSever.py
+++ demoSever.py
@@ -717,7 +717,7 @@ def handle_ComplaintAndCompliment():
     conn = MongoDB(db, 'ComplaintsAndCompliments').get_conn()
     complaint = conn.find_one({'_id': ObjectId(complaintID)})
     conn.update_one(complaint, {'$set': {'status': determination,
-                                         'finalizedDate': pymongo.datetime.datetime.now(),
+                                         'finalizedDate': datetime.datetime.now(),
                                          'reviewBy': userID}})
     if determination == 'accept':
         userID = complaint['toID']
