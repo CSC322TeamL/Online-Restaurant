3eb0c4941f3330d3542b9ebf285b504f75e47fb1
Leji-Li-93 <leji.li.93@outlook.com>
2020-12-06 12:44:16 -0500

    package order list from json response to java object

--- Code/SimpleRestaurant/app/src/main/java/com/example/simplerestaurant/Fragments/UserOrderFragment.java
+++ Code/SimpleRestaurant/app/src/main/java/com/example/simplerestaurant/Fragments/UserOrderFragment.java
@@ -12,11 +12,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simplerestaurant.Interfaces.UserOrderFragmentInterface;
 import com.example.simplerestaurant.R;
+import com.example.simplerestaurant.UnitTools;
+import com.example.simplerestaurant.beans.OrderBean;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class UserOrderFragment extends Fragment {
     private RecyclerView orderRecycler;
     private TextView tvEmptyList;
     private String userID, userType;
+    private ArrayList<OrderBean> displayOrder;
 
     private UserOrderFragmentInterface listener;
 
@@ -25,6 +37,7 @@ public class UserOrderFragment extends Fragment {
         this.listener = listener;
         this.userID = userID;
         this.userType = userType;
+        displayOrder = new ArrayList<>();
     }
 
     @Override
@@ -44,6 +57,47 @@ public class UserOrderFragment extends Fragment {
     }
 
     public void orderListResponse(String res){
-        Log.i("order", res);
+        //Log.i("order", res);
+        try {
+            JSONObject resJson = new JSONObject(res);
+            JSONObject resultJson = resJson.getJSONObject("result");
+            OrderListResponseBean orderListFromResponse =
+                    UnitTools.getGson().fromJson(resultJson.toString().trim(), OrderListResponseBean.class);
+            displayOrder.clear();
+            displayOrder.addAll(orderListFromResponse.getWaiting());
+            displayOrder.addAll(orderListFromResponse.getFinished());
+            Log.i("order", displayOrder.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class OrderListResponseBean{
+        ArrayList<OrderBean> waiting;
+        ArrayList<OrderBean> finished;
+
+        public ArrayList<OrderBean> getWaiting() {
+            return waiting;
+        }
+
+        public void setWaiting(ArrayList<OrderBean> waiting) {
+            this.waiting = waiting;
+        }
+
+        public ArrayList<OrderBean> getFinished() {
+            return finished;
+        }
+
+        public void setFinished(ArrayList<OrderBean> finished) {
+            this.finished = finished;
+        }
+
+        @Override
+        public String toString() {
+            return "OrderListResponseBean{" +
+                    "waiting=" + waiting +
+                    ", finished=" + finished +
+                    '}';
+        }
     }
 }
