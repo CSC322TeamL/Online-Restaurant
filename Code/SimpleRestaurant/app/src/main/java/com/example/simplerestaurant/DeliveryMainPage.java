13fa1538f0cc645ee9b76cef730d338c6108929f
Leji-Li-93 <leji.li.93@outlook.com>
2020-12-08 00:16:01 -0500

    working on delivery person page

---
+++ Code/SimpleRestaurant/app/src/main/java/com/example/simplerestaurant/DeliveryMainPage.java
@@ -0,0 +1,8 @@
+package com.example.simplerestaurant;
+
+public class DeliveryMainPage extends BaseActivity {
+    final private static String SENDING_TAG = "fragment_delivery_sending";
+    final private static String WAITING_TAG = "fragment_delivery_waiting";
+    final private static String FINISHED_TAG = "fragment_delivery_finished";
+    final private static String ACC_TAG = "fragment_user_account";
+}
